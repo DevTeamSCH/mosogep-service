f421af32be4cb5387e34211b7c9f6625e7e93f96
cskrisz <cskrisztianster@gmail.com>
2017-03-29 18:56:10 +0200

    Construct and parse test packets

--- moso.py
+++ moso.py
@@ -5,13 +5,20 @@ from struct import unpack
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 1234))
 
-while 1 :
+while 1:
     data, (address, port) = s.recvfrom(68)
     level = address.split('.')[-1] # last octett of IP is the level number
-    print data
     #drier = str(unpack('>H', data[4:6]))
     #washingMachine = str(unpack('>H', data[6:8]))
     #print washingMachine
     #time.sleep(5)
-    # szaritogep = str(unpack('>H', data[4:6])[0])
-    # mosogep = str(unpack('>H', data[6:8])[0])
+
+    print('address:', address)
+    print('port:', port)
+    print('level:', level)
+
+    print('data:', data)
+    drier, wahing_machine = unpack('>HH', data[4:8])
+    print('szaritogep:', drier)
+    print('mosogep:', wahing_machine)
+    print()
--- sender.py
+++ sender.py
@@ -1,12 +1,18 @@
 import socket
+from struct import pack
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 1234
-MESSAGE = 'Szia'
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+DRIER = 56789
+WASHING_MACHINE = 290
+DATA = pack('>iHH', 1234567890, DRIER, WASHING_MACHINE)  # first 4 bytes is ip
+print('szarito:', DRIER)
+print('mosogep:', WASHING_MACHINE)
+print('data:', DATA)
+
 while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    sock.sendto(DATA, (UDP_IP, UDP_PORT))
